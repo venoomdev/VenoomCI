c817f896e193e8c057cca8a32b485f2253ec8b1e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-09-28 17:18:27 +0530

    Revert "sysctl: Nuke sched_boost feature"
    This reverts commit 14b77211307c27cd6b1f4c3fdc4c68dfd980a628.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -439,7 +439,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &sysctl_sched_group_upmigrate_pct,
 	},
-#if 0
 	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,
@@ -459,7 +458,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
-#endif
 	{
 		.procname	= "sched_conservative_pl",
 		.data		= &sysctl_sched_conservative_pl,
