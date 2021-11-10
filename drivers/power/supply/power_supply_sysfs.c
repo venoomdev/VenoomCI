0e62e97658875e5f49c1bfece86fae8a899d79bc
Jebaitedneko <Jebaitedneko@gmail.com>
2021-11-10 23:58:29 +0530

    treewide: kill some logspams
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -19,8 +19,28 @@
 
 #include "power_supply.h"
 
+//<<<<<<< HEAD
 //#undef dev_dbg
 //#define dev_dbg  dev_err
+//=======
+#undef dev_info
+#define dev_info(x, ...)
+#undef dev_dbg
+#define dev_dbg(x, ...)
+#undef dev_err
+#define dev_err(x, ...)
+#undef pr_info
+#define pr_info(x, ...)
+#undef pr_debug
+#define pr_debug(x, ...)
+#undef pr_error
+#define pr_error(x, ...)
+#undef printk
+#define printk(x, ...)
+#undef printk_deferred
+#define printk_deferred(x, ...)
+
+//>>>>>>> d2008578bfc88 (treewide: kill some logspams)
 /*
  * This is because the name "current" breaks the device attr macro.
  * The "current" word resolves to "(get_current())" so instead of
