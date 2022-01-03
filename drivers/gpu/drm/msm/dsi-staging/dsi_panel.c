c6b0142b946cf13efc2b7a1bee88d9926a8c0d1e
venoom <venoomtulip@gmail.com>
2022-01-03 14:25:03 +0700

    add 130 fps

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1474,14 +1474,18 @@ static int dsi_panel_parse_dfps_caps(struct dsi_panel *panel)
 		goto error;
 	}
 
-	dfps_caps->dfps_list_len = 3;
+	dfps_caps->dfps_list_len = 7;
 
 	dfps_caps->dfps_list = kcalloc(dfps_caps->dfps_list_len, sizeof(u32),
 			GFP_KERNEL);
 
-	dfps_caps->dfps_list[0] = 120;
-	dfps_caps->dfps_list[1] = 90;
-	dfps_caps->dfps_list[2] = 60;
+	dfps_caps->dfps_list[0] = 130;
+	dfps_caps->dfps_list[1] = 120;
+	dfps_caps->dfps_list[2] = 90;
+        dfps_caps->dfps_list[3] = 75;
+        dfps_caps->dfps_list[4] = 60;
+        dfps_caps->dfps_list[5] = 50;
+        dfps_caps->dfps_list[6] = 30;
 
 	dfps_caps->dfps_support = true;
 
