8c5c80fd3372a3d930ada6e9d81eae72310afec1
venoom <venoomtulip@gmail.com>
2022-01-05 13:29:42 +0700

    Revert "try remove some trunk"
    This reverts commit ceeb16310f26d49da8f31a907329eaf02bc62b92.

--- drivers/input/fingerprint/gf3208/gf_spi.h
+++ drivers/input/fingerprint/gf3208/gf_spi.h
@@ -85,6 +85,8 @@ struct gf_ioc_chip_info {
 #define GF_IOC_RESET            _IO(GF_IOC_MAGIC, 2)
 #define GF_IOC_ENABLE_IRQ       _IO(GF_IOC_MAGIC, 3)
 #define GF_IOC_DISABLE_IRQ      _IO(GF_IOC_MAGIC, 4)
+#define GF_IOC_ENABLE_SPI_CLK   _IOW(GF_IOC_MAGIC, 5, uint32_t)
+#define GF_IOC_DISABLE_SPI_CLK  _IO(GF_IOC_MAGIC, 6)
 #define GF_IOC_ENABLE_POWER     _IO(GF_IOC_MAGIC, 7)
 #define GF_IOC_DISABLE_POWER    _IO(GF_IOC_MAGIC, 8)
 #define GF_IOC_ENTER_SLEEP_MODE _IO(GF_IOC_MAGIC, 10)
@@ -101,18 +103,24 @@ struct gf_ioc_chip_info {
 #define  GF_IOC_MAXNR    14  /* THIS MACRO IS NOT USED NOW... */
 #endif
 
+//#define AP_CONTROL_CLK       1
 #define  USE_PLATFORM_BUS     1
+//#define  USE_SPI_BUS	1
+//#define GF_FASYNC   1	/*If support fasync mechanism.*/
 #define GF_NETLINK_ENABLE 1
 #define GF_NET_EVENT_IRQ 1
 #define GF_NET_EVENT_FB_BLACK 2
 #define GF_NET_EVENT_FB_UNBLACK 3
 #define NETLINK_TEST 25
-#define MAX_MSGSIZE 32
 
 struct gf_dev {
 	dev_t devt;
 	struct list_head device_entry;
+#if defined(USE_SPI_BUS)
+	struct spi_device *spi;
+#elif defined(USE_PLATFORM_BUS)
 	struct platform_device *spi;
+#endif
 	struct clk *core_clk;
 	struct clk *iface_clk;
 
@@ -125,32 +133,30 @@ struct gf_dev {
 	int irq;
 	int irq_enabled;
 	int clk_enabled;
+#ifdef GF_FASYNC
+	struct fasync_struct *async;
+#endif
 	struct notifier_block notifier;
 	char device_available;
 	char fb_black;
 	char wait_finger_down;
 	struct work_struct work;
-	struct pinctrl         *gf_pinctrl;
-	struct pinctrl_state   *gpio_state_enable;
-	struct pinctrl_state   *gpio_state_disable;
-	signed enable_gpio;
-	int screen_state;
 };
 
-static inline int gf_pinctrl_init(struct gf_dev* gf_dev);
-static inline int gf_parse_dts(struct gf_dev* gf_dev);
+int gf_parse_dts(struct gf_dev *gf_dev);
+
 static inline void gf_cleanup(struct gf_dev *gf_dev)
 {
-	pr_info("[info] %s\n",__func__);
+	pr_debug("[info] %s\n",__func__);
 	if (gpio_is_valid(gf_dev->irq_gpio))
 	{
 		gpio_free(gf_dev->irq_gpio);
-		pr_info("remove irq_gpio success\n");
+		pr_debug("remove irq_gpio success\n");
 	}
 	if (gpio_is_valid(gf_dev->reset_gpio))
 	{
 		gpio_free(gf_dev->reset_gpio);
-		pr_info("remove reset_gpio success\n");
+		pr_debug("remove reset_gpio success\n");
 	}
 }
 
@@ -158,7 +164,7 @@ static inline int gf_power_on(struct gf_dev *gf_dev)
 {
 	int rc = 0;
 
-	pr_info("---- power on ok ----\n");
+	pr_debug("---- power on ok ----\n");
 
 	return rc;
 }
@@ -166,16 +172,15 @@ static inline int gf_power_off(struct gf_dev *gf_dev)
 {
 	int rc = 0;
 
-    pr_info("---- power off ----\n");
+	pr_debug("---- power off ----\n");
 
 	return rc;
 }
 
-static inline int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms);
-static inline void sendnlmsg(char *msg);
-static inline int netlink_init(void);
-static inline void netlink_exit(void);
-extern int gf_opticalfp_irq_handler(int event);
-extern int opticalfp_irq_handler(struct fp_underscreen_info* tp_info);
-#endif /*__GF_SPI_H*/
+int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms);
+int gf_irq_num(struct gf_dev *gf_dev);
 
+void sendnlmsg(char *message);
+int netlink_init(void);
+void netlink_exit(void);
+#endif /*__GF_SPI_H*/
