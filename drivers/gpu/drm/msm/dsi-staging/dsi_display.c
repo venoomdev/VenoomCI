d61ca74fa1270ceaa619b3a92b036bd300723a66
venoom <venoomtulip@gmail.com>
2022-01-10 15:58:47 +0700

    Revert "drm/msm/dsi-staging: fix power check in idle hints"
    This reverts commit afd60cd2bba5acb4042977dbf8e368b1250f072a.

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -63,12 +63,6 @@ static const struct of_device_id dsi_display_dt_match[] = {
 
 static unsigned int cur_refresh_rate = 60;
 
-static inline bool is_lp_mode(int power_mode)
-{
-	return power_mode == SDE_MODE_DPMS_LP1 ||
-			power_mode == SDE_MODE_DPMS_LP2;
-}
-
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -1113,8 +1107,8 @@ int dsi_display_set_power(struct drm_connector *connector,
 		rc = dsi_panel_set_lp2(display->panel);
 		break;
 	case SDE_MODE_DPMS_ON:
-		if (is_lp_mode(display->panel->power_mode))
-			msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notify_data);
+		if (display->panel->power_mode == SDE_MODE_DPMS_LP1 ||
+			display->panel->power_mode == SDE_MODE_DPMS_LP2)
 			rc = dsi_panel_set_nolp(display->panel);
 		break;
 	case SDE_MODE_DPMS_OFF:
@@ -6775,8 +6769,7 @@ void dsi_display_set_idle_hint(void *dsi_display, bool is_idle)
 	if (unlikely(!display || !display->panel))
 		return;
 
-	if (!dsi_panel_initialized(display->panel) ||
-	    is_lp_mode(display->panel->power_mode))
+	if (display->panel->power_mode != DRM_MODE_DPMS_ON)
 		return;
 
 	if (is_idle)
