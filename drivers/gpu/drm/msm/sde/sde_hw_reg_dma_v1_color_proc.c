4ee924ceeb6e2f49f8084d2ed278261c8fab4773
ekoariffaizal3 <ekoariffaizal3@gmail.com>
2022-02-07 03:04:08 +0000

    Revert "drm/msm/sde: Fix Introduce KCAL color control"
    This reverts commit 5c398c981bf678f8c3a6b07b41a1d85629342c6f.

--- drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
+++ drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
@@ -931,13 +931,7 @@ static void _dspp_pccv4_off(struct sde_hw_dspp *ctx, void *cfg)
 
 void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 {
-	struct sde_hw_reg_dma_ops *dma_ops;
-	struct sde_reg_dma_kickoff_cfg kick_off;
-	struct sde_hw_cp_cfg *hw_cfg = cfg;
-	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
-	struct drm_msm_pcc *pcc_cfg;
-	struct drm_msm_pcc_coeff *coeffs = NULL;
-	u32 *data = NULL;
+	u32 opcode = 0;
 	int rc, i = 0;
 	u32 reg = 0;
 
@@ -1037,7 +1031,10 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	if (rc)
 		DRM_ERROR("failed to kick off ret %d\n", rc);
 
-exit:
+	if (kcal->enabled)
+		reg_dmav1_setup_dspp_pa_hsicv17_kcal(ctx, hw_cfg->ctl);
+
+	exit:
 	kfree(data);
 }
 
@@ -1046,6 +1043,7 @@ void reg_dmav1_setup_dspp_pa_hsicv17(struct sde_hw_dspp *ctx, void *cfg)
 	struct sde_hw_reg_dma_ops *dma_ops;
 	struct sde_reg_dma_kickoff_cfg kick_off;
 	struct sde_hw_cp_cfg *hw_cfg = cfg;
+	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
 	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
 	struct drm_msm_pa_hsic *hsic_cfg;
 	u32 reg = 0, opcode = 0, local_opcode = 0;
