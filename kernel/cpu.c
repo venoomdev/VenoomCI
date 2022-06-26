4acd59a255ce3f0dc63b18b8a0549734364bec84
Moises <moi162@gmail.com>
2022-06-26 13:05:01 +0200

    define the values of all clusters

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2408,6 +2408,26 @@ EXPORT_SYMBOL(__cpu_active_mask);
 struct cpumask __cpu_isolated_mask __read_mostly;
 EXPORT_SYMBOL(__cpu_isolated_mask);
 
+
+
+/*static unsigned long lp_mask_bits = 15;
+static unsigned long big_mask_bits = 112;
+static unsigned long prime_mask_bits = 128;*/
+
+
+static const unsigned long lp_cpu_bits = 15;
+const struct cpumask *const cpu_lp_mask = to_cpumask(&lp_cpu_bits);
+EXPORT_SYMBOL(cpu_lp_mask);
+
+static const unsigned long perf_cpu_bits = 112;
+const struct cpumask *const cpu_perf_mask = to_cpumask(&perf_cpu_bits);
+EXPORT_SYMBOL(cpu_perf_mask);
+
+
+static const unsigned long prime_cpu_bits = 128;
+const struct cpumask *const cpu_prime_mask = to_cpumask(&prime_cpu_bits);
+EXPORT_SYMBOL(cpu_prime_mask);
+
 void init_cpu_present(const struct cpumask *src)
 {
 	cpumask_copy(&__cpu_present_mask, src);
