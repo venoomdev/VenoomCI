43e29a97e181b5aa334c52474c7c069f72f43a43
root <ekoariffaizal3@gmail.com>
2022-06-28 11:45:36 +0530

    msm: vidc: Reduce max_packets count to 480

--- drivers/media/platform/msm/vidc_3x/venus_hfi.c
+++ drivers/media/platform/msm/vidc_3x/venus_hfi.c
@@ -77,7 +77,7 @@ const struct msm_vidc_gov_data DEFAULT_BUS_VOTE = {
 	.imem_size = 0,
 };
 
-const int max_packets = 250;
+const int max_packets = 480; /* 16 sessions x 30 packets */
 
 static void venus_hfi_pm_handler(struct work_struct *work);
 static DECLARE_DELAYED_WORK(venus_hfi_pm_work, venus_hfi_pm_handler);
