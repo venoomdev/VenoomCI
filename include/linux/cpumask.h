c55463834285e6d546f705dc39474839577bf4b8
ekoariffaizal3 <ekoariffaizal3@gmail.com>
2022-07-15 04:28:50 +0000

    venoom regen

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -105,6 +105,7 @@ extern struct cpumask __cpu_perf_mask;
 extern const struct cpumask *const cpu_lp_mask;
 extern const struct cpumask *const cpu_perf_mask;
 extern const struct cpumask *const cpu_perfp_mask;
+extern const struct cpumask *const cpu_prime_mask;
 
 #if NR_CPUS > 1
 #define num_online_cpus()	cpumask_weight(cpu_online_mask)
