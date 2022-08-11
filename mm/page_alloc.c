1053c2c5ca565523fe410ac23b25bdac1aeafe4e
ekoariffaizal3 <ekoariffaizal3@gmail.com>
2022-08-11 02:55:43 +0000

    Revert "mm: vmpressure: Fix rampant inaccuracies caused by stale data usage"
    This reverts commit 4d1ef934675ad6ad09cd3e9d4b1ff77185714af8.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4394,7 +4394,6 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	unsigned int cpuset_mems_cookie;
 	int reserve_flags;
 	bool woke_kswapd = false;
-	bool used_vmpressure = false;
 
 	/*
 	 * We also sanity check to catch abuse of atomic reserves being used by
@@ -4435,8 +4434,6 @@ retry_cpuset:
 			atomic_long_inc(&kswapd_waiters);
 			woke_kswapd = true;
 		}
-		if (!used_vmpressure)
-			used_vmpressure = vmpressure_inc_users(order);
 		wake_all_kswapds(order, ac);
 	}
 
@@ -4527,8 +4524,6 @@ retry:
 		goto nopage;
 
 	/* Try direct reclaim and then allocating */
-	if (!used_vmpressure)
-		used_vmpressure = vmpressure_inc_users(order);
 	page = __alloc_pages_direct_reclaim(gfp_mask, order, alloc_flags, ac,
 							&did_some_progress);
 	if (page)
@@ -4641,8 +4636,6 @@ fail:
 got_pg:
 	if (woke_kswapd)
 		atomic_long_dec(&kswapd_waiters);
-	if (used_vmpressure)
-		vmpressure_dec_users();
 	if (!page)
 		warn_alloc(gfp_mask, ac->nodemask,
 				"page allocation failure: order:%u", order);
