db56f79ac5370034af9bac38a963ee406441a197
ekoariffaizal3 <ekoariffaizal3@gmail.com>
2022-08-11 02:58:32 +0000

    Revert "mm: vmpressure: Interpret zero scanned pages as 100% pressure"
    This reverts commit 9ea82e2c00eb80898362d5e87f3a0c0920102ad3.

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -344,43 +344,28 @@ static void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	struct vmpressure *vmpr = &global_vmpressure;
 	unsigned long pressure;
 
-	if (scanned) {
-		spin_lock(&vmpr->sr_lock);
-		if (!vmpr->scanned)
-			calculate_vmpressure_win();
+	if (!scanned)
+		return;
 
-//	vmpr->scanned += scanned;
-//	vmpr->reclaimed += reclaimed;
-//	scanned = vmpr->scanned;
-//	reclaimed = vmpr->reclaimed;
-//	spin_unlock(&vmpr->sr_lock);
-		vmpr->scanned += scanned;
-		vmpr->reclaimed += reclaimed;
+	spin_lock(&vmpr->sr_lock);
+	if (!vmpr->scanned)
+		calculate_vmpressure_win();
 
-		if (!current_is_kswapd())
-			vmpr->stall += scanned;
+	vmpr->scanned += scanned;
+	vmpr->reclaimed += reclaimed;
+	scanned = vmpr->scanned;
+	reclaimed = vmpr->reclaimed;
+	spin_unlock(&vmpr->sr_lock);
 
-		stall = vmpr->stall;
-		scanned = vmpr->scanned;
-		reclaimed = vmpr->reclaimed;
-		spin_unlock(&vmpr->sr_lock);
-
-		if (scanned < vmpressure_win)
-			return;
-	}
+	if (scanned < vmpressure_win)
+		return;
 
 	spin_lock(&vmpr->sr_lock);
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
 	spin_unlock(&vmpr->sr_lock);
 
-//	pressure = vmpressure_calc_pressure(scanned, reclaimed);
-	if (scanned) {
-		pressure = vmpressure_calc_pressure(scanned, reclaimed);
-		pressure = vmpressure_account_stall(pressure, stall, scanned);
-	} else {
-		pressure = 100;
-	}
+	pressure = vmpressure_calc_pressure(scanned, reclaimed);
 	vmpressure_notify(pressure);
 }
 
