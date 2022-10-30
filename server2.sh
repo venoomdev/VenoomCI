922208b3dba769ad9b2b8da9b953cc480d626b7b
Rohail33 <rohailyousafzai49@gmail.com>
2022-10-30 20:19:06 +0000

    Fix errors for Clang16

--- server2.sh
+++ server2.sh
@@ -1,12 +1,12 @@
 set -e
 ## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=vendor/alioth_defconfig
-export PATH="/home/rishi/kernel/clang/bin:$PATH"
+export PATH="/home/rishi/kernel/clang16/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="$(/home/rishi/kernel/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export KBUILD_COMPILER_STRING="$(/home/rishi/kernel/clang16/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-if ! [ -d "/home/rishi/kernel/clang" ]; then
+if ! [ -d "/home/rishi/kernel/clang16" ]; then
 echo "Proton clang not found! Cloning..."
 if ! git clone -q https://gitlab.com/ZyCromerZ/clang.git --depth=1 --single-branch /home/rohail/kernel/proton-clang; then
 echo "Cloning failed! Aborting..."
@@ -34,6 +34,5 @@ make -j$(nproc --all) O=out \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       NM=llvm-nm \
-                      OBJCOPY=llvm-objcopy \
                       OBJDUMP=llvm-objdump \
                       STRIP=llvm-strip
