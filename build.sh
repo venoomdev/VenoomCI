5b77a6edd6150b7cbf60b4e3b6c7448556d10b2d
DandaGaharu <dandagaharu77@gmail.com>
2022-11-17 08:34:19 +0700

    remove patch

--- build.sh
+++ build.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
 
-upstream() {
-if [ -f $PWD/*.patch ]; then
-	clear
-	echo " ";
-	echo -e "**** Patching kernel ****";
-	echo " ";
-	patch -N -p1 < *.patch
-	rm -f $PWD/*.patch
-	#clear
-	echo " ";
-	echo -e "$blue**** Patching kernel finished ****";
-else
-	clear
-	echo " ";
-	echo -e "**** Patch kernel no found ****";
-	echo " ";
-	echo "Make patch file from diff, and place into Kernel dir ";
-	echo -e "with name oxygen.patch";
-fi
-}
-
-
-upstream;
 
 if [[-d "out"]]
 then
