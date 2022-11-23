1daea46a7f49fef0bb3133ad62a0dd375e63c6af
diphons <diphons@gmail.com>
2022-11-23 19:38:25 +0700

    apply name build

--- init/version.c
+++ init/version.c
@@ -43,7 +43,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
-	"Linux version " UTS_RELEASE " ($USER)"
+	"Linux version " UTS_RELEASE " (diphons)"
 	"(" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char *linux_banner_ptr = linux_banner;
@@ -51,7 +51,7 @@ EXPORT_SYMBOL_GPL(linux_banner_ptr);
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" ($USER)"
+	" (diphons)"
 	" (" LINUX_COMPILER ") %s\n";
 
 BUILD_SALT;
