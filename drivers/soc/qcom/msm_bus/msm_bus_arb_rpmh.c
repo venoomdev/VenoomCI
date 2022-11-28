d2192544b7b9cc0e43aa4976a6105747e4d1e127
diphons <diphons@gmail.com>
2022-11-28 10:03:12 +0700

    Cleanup miss merge

--- drivers/soc/qcom/msm_bus/msm_bus_arb_rpmh.c
+++ drivers/soc/qcom/msm_bus/msm_bus_arb_rpmh.c
@@ -1630,14 +1630,9 @@ static int update_request_adhoc(uint32_t cl, unsigned int index)
 	if (pdata->alc)
 		ret = update_client_alc(client, index);
 	else {
-<<<<<<< HEAD
 		if (!oops_in_progress)
 			msm_bus_dbg_client_data(client->pdata, index, cl);
-		ret = update_client_paths(client, log_transaction, index);
-=======
-		msm_bus_dbg_client_data(client->pdata, index, cl);
 		ret = update_client_paths(client, index);
->>>>>>> e175b63a7e1b... msm: msm_bus: Kill transaction logging functionality
 	}
 	if (ret) {
 		pr_err("%s: Err updating path\n", __func__);
