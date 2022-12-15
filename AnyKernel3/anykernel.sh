783af63cb101018b27565042690e2f3c763f0064
ekoariffaizal3 <ekoariffaizal3@gmail.com>
2022-12-15 09:04:41 +0000

    add 4

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=InfiniR Kernel by raystef66
+kernel.string=lemper Kernel by Pocongtobat
 do.devicecheck=1
 do.modules=0
 do.systemless=1
