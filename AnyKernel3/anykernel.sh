20b7a5b28be7ab3a857dbc70e32653a82cd72bd2
piraterex <piraterex.gm@gmail.com>
2022-12-18 22:15:59 +0530

    fixup

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Black Panda Kernel by piraterex
+kernel.string=Panguin Kernel by piraterex
 do.devicecheck=1
 do.modules=0
 do.systemless=1
