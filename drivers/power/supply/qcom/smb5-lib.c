af855c0ed8f2bd4e3f123ec5632218351e4fc07a
NotZeetaa <rodrigo2005contente@gmail.com>
2023-01-01 23:14:55 +0000

    Revert "treewide: kill some logspams"
    This reverts commit 742164dcef89afb1a4e01b6d117599c6d1cce50f.

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -26,11 +26,14 @@
 #endif
 
 #define smblib_err(chg, fmt, ...)		\
-	pr_err("%s: %s: " fmt, chg->name,	\
+	pr_debug("%s: %s: " fmt, chg->name,	\
 		__func__, ##__VA_ARGS__)	\
 
-#define smblib_dbg(chg, reason, fmt, ...)			\
-	do { } while (0)
+#define smblib_dbg(chg, reason, fmt, ...)		\
+	do {						\
+		pr_debug("%s: %s: " fmt, chg->name,	\
+			__func__, ##__VA_ARGS__);	\
+	} while (0)
 
 #define typec_rp_med_high(chg, typec_mode)			\
 	((typec_mode == POWER_SUPPLY_TYPEC_SOURCE_MEDIUM	\
