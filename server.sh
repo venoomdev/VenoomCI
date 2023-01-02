29ece50f3a528de3490d9e4fd7df9f52b0caa636
Rohail33 <rohailyousafzai49@gmail.com>
2023-01-02 16:23:18 +0000

    Build: Fix path

--- server.sh
+++ server.sh
@@ -2,12 +2,12 @@
 #set -e
 ## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=vendor/alioth_defconfig
-export PATH="/root/rohail33/kernel/clang/bin:$PATH"
+export PATH="/home/ubuntu/rohail33/kernel/clang/bin:$PATH"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_COMPILER_STRING="$(/root/rohail33/kernel/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export KBUILD_COMPILER_STRING="$(/home/ubuntu/rohail33/kernel/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
-if ! [ -d "/root/rohail33/kernel/clang" ]; then
+if ! [ -d "/home/ubuntu/rohail33/kernel/clang" ]; then
 echo "Proton clang not found! Cloning..."
 if ! git clone -q https://gitlab.com/ZyCromerZ/clang.git --depth=1 --single-branch /home/rohail/kernel/proton-clang; then
 echo "Cloning failed! Aborting..."
